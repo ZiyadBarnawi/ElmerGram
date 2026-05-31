8fda0c53b57959893c5aaa217ba1c538ce1a2424
Ziyad Barnawi <ziyad0barnawi@outlook.com>
2026-05-31 22:20:50 +0300

    - New color theme - Lodash JS library to shuffle use posts in postsBody comp - Small CSS changes to the postsBody compt

--- angular-instagram-profile/src/app/app.config.ts
+++ angular-instagram-profile/src/app/app.config.ts
@@ -32,7 +32,6 @@ export const appConfig: ApplicationConfig = {
         preset: CustomLaraPreset,
       },
       ripple: true,
-      // ripple: true,
     }),
     UserService,
     MessageService,
--- angular-instagram-profile/src/app/shared/components/profile-body/profileBody.component.ts
+++ angular-instagram-profile/src/app/shared/components/profile-body/profileBody.component.ts
@@ -2,21 +2,34 @@ import { AfterViewInit, Component, inject, input, signal } from '@angular/core';
 
 import { TabsModule } from 'primeng/tabs';
 
-import { Post } from '../post/post.component';
 import { UserService } from '@core/services/user.service';
 import { Avatar } from 'primeng/avatar';
+import { shuffle } from 'lodash';
+import { Post as PostModel } from '@shared/models/post.model';
+import { Post } from '../post/post.component';
 @Component({
   standalone: true,
   selector: 'app-profile-body',
-  imports: [TabsModule, Post, Avatar],
+  imports: [TabsModule, Avatar, Post],
   templateUrl: './profileBody.component.html',
   styleUrl: './profileBody.component.css',
 })
 export class ProfileBodyComponent {
-  randomNum = signal<number>(Math.floor(Math.random() * 7));
   userService = inject(UserService);
   user = this.userService.user;
-  updateRandomValue(): void {
-    this.randomNum.set(Math.floor(Math.random() * 7) + 1);
+  shuffledSavedPosts: Array<PostModel> = [];
+  shuffledPosts: Array<PostModel> = [];
+
+  ngOnInit() {
+    if (this.user()?.posts) {
+      this.shuffledSavedPosts = shuffle([...this.user()?.posts!]).slice(
+        0,
+        Math.floor(Math.random() * 7) + 1,
+      );
+      this.shuffledPosts = shuffle([...this.user()?.posts!]).slice(
+        0,
+        Math.floor(Math.random() * 7) + 1,
+      );
+    }
   }
 }
